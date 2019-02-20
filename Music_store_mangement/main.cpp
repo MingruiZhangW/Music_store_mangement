4f8f898372f2df9ca6a72779483bc9d0095b2f53
MingruiZhangW <46324965+MingruiZhangW@users.noreply.github.com>
2019-02-19 21:36:29 -0500

    Main loop

--- Music_store_mangement/main.cpp
+++ Music_store_mangement/main.cpp
@@ -13,12 +13,49 @@ int main()
 {
 	DBconnection_Init();
 
-	//AddNewItemInDatabase();
-	//ShowAllItems(false);
-	//FindMusic();
-	//EditItem();
-	//RemoveItem();
-	CreateOrder();
-	std::cin.get();
+	std::cout << "Welcome to the Music Store Database Mangement System!" << std::endl;
+
+	while (true) {
+
+		std::cout << std::endl;
+		std::cout << "1. Show All Items in the Store " << std::endl;
+		std::cout << std::endl;
+		std::cout << "2. Add New Item in Database " << std::endl;
+		std::cout << std::endl;
+		std::cout << "3. Items in Stock that are Available" << std::endl;
+		std::cout << std::endl;
+		std::cout << "4. Find Specific Music" << std::endl;
+		std::cout << std::endl;
+		std::cout << "5. Edit Item" << std::endl;
+		std::cout << std::endl;
+		std::cout << "6. Remove Item" << std::endl;
+		std::cout << std::endl;
+		std::cout << "7. Create An Order" << std::endl;
+		std::cout << std::endl;
+		std::cout << "0. Exit Program" << std::endl;
+		std::cout << std::endl;
+
+		std::cout << "Please Select One of the Options , Exit -> 0" << std::endl;
+
+		int selection_vode;
+		getinput_num(selection_vode);
+		int exit;
+
+		switch (selection_vode)
+		{
+		case 0: exit = 1;  break;
+		case 1: ShowAllItems(false);  break;
+		case 2: AddNewItemInDatabase();  break;
+		case 3: ItemInStock();  break;
+		case 4: FindMusic();  break;
+		case 5: EditItem();  break;
+		case 6: RemoveItem();  break;
+		case 7: CreateOrder();  break;
+		}
+
+		if (exit == 1) { break;  }
+	
+	}
+
 	return 0;
 }
